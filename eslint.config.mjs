2305b26281da77351915a9a3464eb8d2a511a994
Tania Lapa <taniamiroshnuchenko@gmail.com>
2026-08-22 15:30:19 +0200

    Fix project configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+];
